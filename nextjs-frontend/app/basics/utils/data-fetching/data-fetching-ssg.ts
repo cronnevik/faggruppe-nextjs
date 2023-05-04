78c6fc97aa51548c656e998f3509ed5c3b2782ef
Christian Rønnevik <cronnevik@gmail.com>
2023-05-04 11:14:16 +0200

    fjerne logging av host

--- nextjs-frontend/app/basics/utils/data-fetching/data-fetching-ssg.ts
+++ nextjs-frontend/app/basics/utils/data-fetching/data-fetching-ssg.ts
@@ -8,7 +8,6 @@ import { notFound } from "next/navigation";
 export const hentAlleKonsulenterSSG = async(): Promise<Konsulent[]> => {
     // By default, fetch will automatically fetch and cache data indefinitely. 'force-cache' is the default
     // To fetch fresh data on every fetch request, use the cache: 'no-store' option. 
-    console.log("host: ", process.env.NEXT_PUBLIC_API_HOST)
     const resultat = await fetch(`http://localhost:3050/api/konsulenter`, { cache: 'no-store' });
     if (!resultat.ok) {
         // This will activate the closest `error.` Error Boundary
