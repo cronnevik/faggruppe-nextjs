b6e887979299380e2f741044b865ed24c6490580
Christian Rønnevik <cronnevik@gmail.com>
2023-05-10 13:45:45 +0200

    fjerne test div

--- nextjs-basics/app/page.tsx
+++ nextjs-basics/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <main>
-      <div style={{height: '100px', width: '100px', border: '1px solid black'}} className="bg-primary-main"></div>
+      <div>Home page</div>
     </main>
   )
 }
